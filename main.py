67f2875244029be405023996edf6eb90ff1a80df
Gary <58466698+garypach@users.noreply.github.com>
2021-12-09 22:12:21 -0800

    pydantic schema

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content:str
+    published: bool = True
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World2"}
+
+@app.post("/createpost")
+async def get_posts(post:Post):
+    print(post.dict())
+    return {"data": post.dict()}
+
+
+#title str, content str
