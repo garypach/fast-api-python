8cc605b6c72b3214cbbb6882758ead15a4824db9
Gary <58466698+garypach@users.noreply.github.com>
2021-12-11 14:17:17 -0800

    mock posts

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from random import randrange
 app = FastAPI()
 
 class Post(BaseModel):
@@ -8,14 +8,33 @@ class Post(BaseModel):
     content:str
     published: bool = True
 
+my_posts = [{"title": "title of post1", "content":"content1","id": 1},{"title": "title of post2", "content":"content2","id": 2} ]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 @app.get("/")
 async def root():
     return {"message": "Hello World2"}
 
-@app.post("/createpost")
+@app.get("/posts")
+async def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts")
 async def get_posts(post:Post):
-    print(post.dict())
-    return {"data": post.dict()}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,100000)
+    my_posts.append(post_dict)
+    return {"data": post}
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    return{"post_detail": post}
 
 
 #title str, content str
